211a8e266d6df18701464209c334f93897d0a734
thierryotis <thierryotis@yahoo.fr>
2023-11-08 03:52:01 +0100

    Update app.js
    deleted connection to database

--- app.js
+++ app.js
@@ -31,24 +31,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 
-const connectDatabase = async () => {
-  try {
-    const connection = await mysql.createConnection({
-      host: "51.254.38.237",
-      port: 3306,
-      user: "ntsendan_db",
-      password: "XN5KFw5hiqsefiIf",
-      database: "ntsendan",
-    });
 
-    console.log('Connected to the database!');
-    return connection;
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    process.exit(1);
-  }
-};
-connectDatabase()
 // simple route
 app.get("/", (req, res) => {
   console.log(req.body);
